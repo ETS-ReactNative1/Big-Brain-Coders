e8d42fe82be31f84c974a6e6a80fb85e55a5abc3
Eric Lam <53859607+airyclam@users.noreply.github.com>
2021-10-29 11:28:44 -1000

    camera works, need to add upload

--- app/imports/ui/pages/Landing.jsx
+++ app/imports/ui/pages/Landing.jsx
@@ -1,8 +1,60 @@
+/* eslint-disable no-undef */
+// import { Meteor } from 'meteor/meteor';
 import React from 'react';
-import { Grid, Image } from 'semantic-ui-react';
-
+import { Grid, Image, Button } from 'semantic-ui-react';
 /** A simple static component to render some text for the landing page. */
 class Landing extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      image: null,
+    };
+  }
+
+  setOptions = (srcType) => {
+    /**  Helper function to set options required for cordova-plugin-camera */
+    const options = {
+      // Some common settings are 20, 50, and 100
+      quality: 50,
+      // eslint-disable-next-line no-undef
+      destinationType: Camera.DestinationType.FILE_URI,
+      // In this app, dynamically set the picture source, Camera or photo gallery
+      sourceType: srcType,
+      encodingType: Camera.EncodingType.JPEG,
+      mediaType: Camera.MediaType.PICTURE,
+      correctOrientation: true,
+    };
+    return options;
+  }
+
+  openCamera = () => {
+    /** Function that creates opens the CAMERA and sets the state to the image
+     * NEED TO POST IMAGE TO CLOUDINARY AND SET STATE TO THAT URL INSTEAD OF LOCAL FILE */
+    const srcType = Camera.PictureSourceType.CAMERA;
+    const options = this.setOptions(srcType);
+
+    navigator.camera.getPicture(cameraSuccess = (imageUri) => {
+      this.setState({ image: imageUri });
+    }, cameraError = (error) => {
+      console.debug(`Unable to obtain picture: ${error}`, 'app');
+
+    }, options);
+  }
+
+  openGallery = () => {
+    /** Function that creates opens the GALLERY and sets the state to the image
+     * NEED TO POST IMAGE TO CLOUDINARY AND SET STATE TO THAT URL INSTEAD OF LOCAL FILE */
+    const srcType = Camera.PictureSourceType.PHOTOLIBRARY;
+    const options = this.setOptions(srcType);
+
+    navigator.camera.getPicture(cameraSuccess = (imageUri) => {
+      this.setState({ image: imageUri });
+    }, cameraError = (error) => {
+      console.debug(`Unable to obtain picture: ${error}`, 'app');
+
+    }, options);
+  }
+
   render() {
     return (
         <Grid verticalAlign='middle' textAlign='center' container>
@@ -13,9 +65,19 @@ class Landing extends React.Component {
 
           <Grid.Column width={8}>
             <h1>Welcome to this template</h1>
+            <Button
+                attached='bottom'
+                content='Take a picture'
+                onClick={this.openCamera}
+            />
+            <Button
+                attached='bottom'
+                content='Upload an image from gallery.'
+                onClick={this.openGallery}
+            />
+            <Image size='small' src={this.state.image} />
             <p>Now get to work and modify this app!</p>
           </Grid.Column>
-
         </Grid>
     );
   }
--- app/mobile-config.js
+++ app/mobile-config.js
@@ -1,6 +1,6 @@
 /* eslint-disable */
 // This section sets up some basic app metadata, the entire section is optional.
-
+// For dev testing, remove for deploy
 App.appendToConfig(`
     <edit-config file="app/src/main/AndroidManifest.xml" mode="merge" target="/manifest/application" xmlns:android="http://schemas.android.com/apk/res/android">
         <application android:usesCleartextTraffic="true"></application>
