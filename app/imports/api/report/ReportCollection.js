2822357f023b4d84caa8c809fc22bcecaee289ac
Glen Larita <glarita@hawaii.edu>
2021-10-31 17:41:47 -1000

    making changes to API

---
+++ app/imports/api/report/ReportCollection.js
@@ -0,0 +1,219 @@
+import { Meteor } from 'meteor/meteor';
+import SimpleSchema from 'simpl-schema';
+import { check } from 'meteor/check';
+import { _ } from 'meteor/underscore';
+import { Roles } from 'meteor/alanning:roles';
+import BaseCollection from '../base/BaseCollection';
+
+export const reportPublications = {
+  report: 'Report',
+  reportAdmin: 'ReportAdmin',
+};
+
+class ReportCollection extends BaseCollection {
+  constructor() {
+    super('Reports', new SimpleSchema({
+      date: String,
+      latitude: Number,
+      longitude: String,
+      island: String,
+      beachName: {
+        type: String,
+        optional: true,
+      },
+      description: {
+        type: String,
+        optional: true,
+      },
+      animal: String,
+      characteristics: String,
+      behavior: String,
+      numOfBeachgoers: Number,
+      name: String,
+      phoneNumber: String,
+      imageUrl: String,
+    }));
+  }
+
+  /**
+   * Defines a new Report item.
+   * @param date the name of the item.
+   * @param latitude how many.
+   * @param longitude the owner of the item.
+   * @param island the owner of the item.
+   * @param beachName the owner of the item.
+   * @param description the owner of the item.
+   * @param animal the owner of the item.
+   * @param characteristics the owner of the item.
+   * @param behavior the owner of the item.
+   * @param numOfBeachgoers the owner of the item.
+   * @param name the owner of the item.
+   * @param phoneNumber the condition of the item.
+   * @param imageUrl the condition of the item.
+   * @return {String} the docID of the new document.
+   */
+  define({
+           date,
+           latitude,
+           longitude,
+           island,
+           beachName,
+           description,
+           animal,
+           characteristics,
+           behavior,
+           numOfBeachgoers,
+           name,
+           phoneNumber,
+           imageUrl,
+         }) {
+    const docID = this._collection.insert({
+      date,
+      latitude,
+      longitude,
+      island,
+      beachName,
+      description,
+      animal,
+      characteristics,
+      behavior,
+      numOfBeachgoers,
+      name,
+      phoneNumber,
+      imageUrl,
+    });
+    return docID;
+  }
+
+  /**
+   * Updates the given document.
+   * @param docID the id of the document to update.
+   * @param name the new name (optional).
+   * @param quantity the new quantity (optional).
+   * @param condition the new condition (optional).
+   */
+  update(docID, {
+    date,
+    latitude,
+    longitude,
+    island,
+    beachName,
+    description,
+    animal,
+    characteristics,
+    behavior,
+    numOfBeachgoers,
+    name,
+    phoneNumber,
+    imageUrl,
+  }) {
+    const updateData = {};
+    if (date) {
+      updateData.date = date;
+    }
+    if (latitude) {
+      updateData.latitude = latitude;
+    }
+    if (longitude) {
+      updateData.longitude = longitude;
+    }
+    if (island) {
+      updateData.island = island;
+    }
+    if (beachName) {
+      updateData.beachName = beachName;
+    }
+    if (description) {
+      updateData.description = description;
+    }
+    if (animal) {
+      updateData.animal = animal;
+    }
+    if (characteristics) {
+      updateData.characteristics = characteristics;
+    }
+    if (behavior) {
+      updateData.behavior = behavior;
+    }
+    // if (numOfBeachgoers) { NOTE: 0 is falsy so we need to check if the numOfBeachgoers is a number.
+    if (_.isNumber(numOfBeachgoers)) {
+      updateData.numOfBeachgoers = numOfBeachgoers;
+    }
+    if (name) {
+      updateData.name = name;
+    }
+    if (phoneNumber) {
+      updateData.phoneNumber = phoneNumber;
+    }
+    if (imageUrl) {
+      updateData.imageUrl = imageUrl;
+    }
+    this._collection.update(docID, { $set: updateData });
+  }
+
+  /**
+   * A stricter form of remove that throws an error if the document or docID could not be found in this collection.
+   * @param { String | Object } name A document or docID in this collection.
+   * @returns true
+   */
+  removeIt(name) {
+    const doc = this.findDoc(name);
+    check(doc, Object);
+    this._collection.remove(doc._id);
+    return true;
+  }
+
+  /**
+   * Default publication method for entities.
+   * It publishes the entire collection for admin and just the reports associated to an owner.
+   */
+  publish() {
+    if (Meteor.isServer) {
+      // get the ReportCollection instance.
+      const instance = this;
+      /** This subscription publishes only the documents associated with the logged in user */
+      Meteor.publish(reportPublications.report, function publish() {
+        if (this.userId) {
+          const username = Meteor.users.findOne(this.userId).username;
+          return instance._collection.find({ owner: username });
+        }
+        return this.ready();
+      });
+
+      /** This subscription publishes all documents regardless of user, but only if the logged in user is the Admin. */
+      Meteor.publish(reportPublications.reportAdmin, function publish() {
+        if (this.userId && Roles.userIsInRole(this.userId, 'admin')) {
+          return instance._collection.find();
+        }
+        return this.ready();
+      });
+    }
+  }
+
+  /**
+   * Subscription method for report owned by the current user.
+   */
+  subscribeReport() {
+    if (Meteor.isClient) {
+      return Meteor.subscribe(reportPublications.report);
+    }
+    return null;
+  }
+
+  /**
+   * Subscription method for admin users.
+   * It subscribes to the entire collection.
+   */
+  subscribeReportAdmin() {
+    if (Meteor.isClient) {
+      return Meteor.subscribe(reportPublications.reportAdmin);
+    }
+    return null;
+  }
+
+}
+
+/**
+ * Provides the singleton instance of this class to all other entities.
+ */
+export const Reports = new ReportCollection();
