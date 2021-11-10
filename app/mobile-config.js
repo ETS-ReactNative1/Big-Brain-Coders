25704681829519da50fccddc449e5f66839017d7
Eric Lam <53859607+airyclam@users.noreply.github.com>
2021-11-10 10:38:17 -1000

    app name change

--- app/mobile-config.js
+++ app/mobile-config.js
@@ -1,5 +1,12 @@
 /* eslint-disable */
 // This section sets up some basic app metadata, the entire section is optional.
+App.info({
+  id: 'com.meteor.coastar',
+  name: 'Coastar',
+  version: "0.0.1"
+});
+
+App.icons('icons/turtle.jpg');
 // For dev testing, remove for deploy
 App.appendToConfig(`
     <edit-config file="app/src/main/AndroidManifest.xml" mode="merge" target="/manifest/application" xmlns:android="http://schemas.android.com/apk/res/android">
