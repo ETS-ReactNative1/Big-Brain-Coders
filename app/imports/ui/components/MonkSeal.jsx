8de667ff757ffb49ab6cbab6a939348c33e85c22
Glen Larita <glarita@hawaii.edu>
2021-11-25 16:22:41 -1000

    small changes

--- app/imports/ui/components/MonkSeal.jsx
+++ app/imports/ui/components/MonkSeal.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Container, Header, Segment } from 'semantic-ui-react';
+import { Container, Header, Image, Segment } from 'semantic-ui-react';
 import { withRouter } from 'react-router-dom';
 
 /** Renders a single row in the List Stuff table. See pages/ListStuff.jsx. */
@@ -9,6 +9,7 @@ class MonkSeal extends React.Component {
         <Container>
           <Segment>
             <Header as='h1'> Hawaiian Monk Seal</Header>
+            <Image size='large' src='../images/seal.jpeg'/>
           </Segment>
         </Container>
     );
